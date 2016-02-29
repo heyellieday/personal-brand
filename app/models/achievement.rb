a02313bebfa29724c0160bc325a94395b83968e4
Ellie Day <heyellieday@gmail.com>
2016-02-29 10:45:01 -0800

    Add position to achievement

--- app/models/achievement.rb
+++ app/models/achievement.rb
@@ -1,4 +1,10 @@
 class Achievement < ActiveRecord::Base
 	extend FriendlyId
 	friendly_id :name, use: :slugged
+
+	acts_as_list
+
+	def self.ordered_by_position
+		order(position: :asc)
+	end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150810234939) do
+ActiveRecord::Schema.define(version: 20160229183900) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 20150810234939) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.string   "date"
+    t.integer  "position"
   end
 
   add_index "achievements", ["slug"], name: "index_achievements_on_slug", using: :btree
